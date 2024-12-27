ab38963da8bc4ee7d27808d615f3e90995c101a5
Cody <rodericgickle@gmail.com>
2024-12-26 22:17:00 -0800

    Mod

---
+++ Cabbage/Cabbage/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace Cabbage.Cabbage
+{
+    public class Constants
+    {
+        public const string GUID = "com.cody.gorillatag.CabbageMural";
+        public const string Name = "CabbageMural";
+        public const string Version = "1.0.0";
+    }
+}
---
+++ Cabbage/Cabbage/Main.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace Cabbage.Cabbage
+{
+    [BepInPlugin(Constants.GUID, Constants.Name, Constants.Version)]
+    public class Main : BaseUnityPlugin
+    {
+        public static AssetBundle bundle;
+        public static GameObject assetBundleParent;
+        public static string assetBundleName = "cabbage";
+        public static string parentName = "BundleParent (put objects in here DONT MOVE)";
+
+        void Start()
+        {
+            GorillaTagger.OnPlayerSpawned(Initialized);
+        }
+
+        void Initialized()
+        {
+            bundle = LoadAssetBundle("Cabbage.Assets." + assetBundleName);
+            assetBundleParent = Instantiate(bundle.LoadAsset<GameObject>(parentName));
+            assetBundleParent.transform.position = new Vector3(-67.2225f, 11.56f, -82.611f);
+            assetBundleParent.name = assetBundleName;
+        }
+
+        public AssetBundle LoadAssetBundle(string path)
+        {
+            Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path);
+            AssetBundle bundle = AssetBundle.LoadFromStream(stream);
+            stream.Close();
+            return bundle;
+        }
+    }
+}
